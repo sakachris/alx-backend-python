450746b7b3a3932360ec9e2a01eb2eea2e606bc2
sakachris <sakachris90@mail.com>
2024-02-04 21:21:24 +0300

    mock http calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,6 +8,9 @@ from typing import Any, Mapping, Sequence, Dict, Tuple
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    '''
+    Test access Nested Map class
+    '''
 
     @parameterized.expand([
         ({"a": 1}, ["a"], 1),
@@ -36,3 +39,24 @@ class TestAccessNestedMap(unittest.TestCase):
         ''' test access nested map exception '''
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    '''
+    Test get Json class
+    '''
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(
+            self,
+            test_url: str,
+            test_payload: Dict,
+            ) -> None:
+        ''' test get json '''
+        mock_attrs = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**mock_attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
