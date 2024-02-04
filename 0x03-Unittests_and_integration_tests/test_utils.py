a40cfc066fec6cc112d1f14656bf5a0b05da603e
sakachris <sakachris90@mail.com>
2024-02-04 21:06:35 +0300

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
-from typing import Any, Mapping, Sequence
+from typing import Any, Mapping, Sequence, Dict, Tuple
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -20,22 +20,19 @@ class TestAccessNestedMap(unittest.TestCase):
         path: Sequence,
         expected_result: Any
     ) -> None:
+        ''' test access nested map '''
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
     @parameterized.expand([
-        ({}, ["a"], KeyError, "Key 'a' not found in the nested map"),
-        (
-            {"a": 1}, ["a", "b"], KeyError,
-            "Key 'b' not found in the nested map"
-        ),
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
     ])
     def test_access_nested_map_exception(
-        self,
-        nested_map: Mapping[str, Any],
-        path: Sequence,
-        expected_exception: Exception,
-        expected_message: str
-    ) -> None:
-        with self.assertRaises(expected_exception) as context:
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            exception: Exception,
+            ) -> None:
+        ''' test access nested map exception '''
+        with self.assertRaises(exception):
             access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), expected_message)
