bb770025f93954e3bc9b94e43d39a6e6d6456d0d
sakachris <sakachris90@gmail.com>
2025-06-11 07:24:37 +0300

    deleting user

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,7 @@
 # messaging/signals.py
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 from .models import Message, Notification, MessageHistory
 
 
@@ -25,3 +26,16 @@ def log_message_edit(sender, instance, **kwargs):
             instance.edited = True
     except Message.DoesNotExist:
         pass
+
+
+@receiver(post_delete, sender=User)
+def cleanup_user_data(sender, instance, **kwargs):
+    # Messages sent or received
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+
+    # Notifications
+    Notification.objects.filter(user=instance).delete()
+
+    # MessageHistory is deleted via CASCADE on Message
+    # No need to delete it explicitly
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
-# Create your views here.
+@login_required
+def delete_user(request):
+    user = request.user
+    logout(request)  # Logs out before deletion
+    user.delete()
+    return redirect('account_deleted')  # Create this template/page
