4b16b76d5eb78e60e9127eb5dece7cf025a637c6
sakachris <sakachris90@gmail.com>
2025-06-11 07:36:21 +0300

    parent message view

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,8 +1,11 @@
+# messaging/views.py
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
+from django.http import HttpResponseRedirect
 from .models import Message
 
 
@@ -25,3 +28,35 @@ def inbox(request):
         .order_by("-timestamp")
     )
     return render(request, "messaging/inbox.html", {"messages": messages})
+
+
+@require_POST
+@login_required
+def send_message(request):
+    receiver_id = request.POST.get("receiver_id")
+    content = request.POST.get("content")
+    parent_message_id = request.POST.get("parent_message_id")  # optional
+
+    if not (receiver_id and content):
+        return redirect("inbox")  # or return an error
+
+    try:
+        receiver = User.objects.get(id=receiver_id)
+    except User.DoesNotExist:
+        return redirect("inbox")  # or return an error
+
+    parent_message = None
+    if parent_message_id:
+        try:
+            parent_message = Message.objects.get(id=parent_message_id)
+        except Message.DoesNotExist:
+            parent_message = None  # silently ignore
+
+    Message.objects.create(
+        sender=request.user,
+        receiver=receiver,
+        content=content,
+        parent_message=parent_message,
+    )
+
+    return HttpResponseRedirect("/messaging/inbox/")
